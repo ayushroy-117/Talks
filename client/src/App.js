00de0f057fe8d8cad82d97419b1e11e5524c07d0
Ayush Roy <royayushgob@gmail.com>
2023-12-18 00:57:09 +0530

    update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,34 @@
+import axios from 'axios';
 import './App.css';
-import Post from "./post";
-import Header from "./header";
-import {Route, Routes} from "react-router-dom";
-import Layout from "./Layout";
-import IndexPage from "./Pages/IndexPage";
-import LoginPage from "./Pages/LoginPage";
-import RegisterPage from "./Pages/RegisterPage";
-import {UserContextProvider} from "./UserContext";
-import CreatePost from "./Pages/CreatePost";
-import PostPage from "./Pages/PostPage";
-import EditPost from "./Pages/EditPost";
+import Post from './post';
+import Header from './header';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './Layout';
+import IndexPage from './Pages/IndexPage';
+import LoginPage from './Pages/LoginPage';
+import RegisterPage from './Pages/RegisterPage';
+import { UserContextProvider } from './UserContext';
+import CreatePost from './Pages/CreatePost';
+import PostPage from './Pages/PostPage';
+import EditPost from './Pages/EditPost';
+
+const backendUrl = 'https://talksss.vercel.app/'; // Replace with your actual backend URL
+
+const axiosInstance = axios.create({
+  baseURL: `${backendUrl}/api`, // Adjust the path if your API is not at the root level
+});
 
 function App() {
   return (
     <UserContextProvider>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<IndexPage />} />
+          <Route index element={<IndexPage axiosInstance={axiosInstance} />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/create" element={<CreatePost />} />
-          <Route path="/post/:id" element={<PostPage />} />
-          <Route path="/edit/:id" element={<EditPost />} />
+          <Route path="/create" element={<CreatePost axiosInstance={axiosInstance} />} />
+          <Route path="/post/:id" element={<PostPage axiosInstance={axiosInstance} />} />
+          <Route path="/edit/:id" element={<EditPost axiosInstance={axiosInstance} />} />
         </Route>
       </Routes>
     </UserContextProvider>
