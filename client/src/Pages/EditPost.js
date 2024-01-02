bb68a643ebd008824d186c0a95a765003895b321
Ayush Roy <84558759+ayushroy-117@users.noreply.github.com>
2024-01-03 00:35:08 +0530

    Update EditPost.js

--- client/src/Pages/EditPost.js
+++ client/src/Pages/EditPost.js
@@ -31,7 +31,7 @@ export default function EditPost() {
         if (files?.[0]) {
             data.set('file', files?.[0]);
         }
-        const response = await fetch('https://talks-ashen.vercel.app/post', {
+        const response = await fetch('https://talksss-ayushroy-117.vercel.app/post', {
             method: 'PUT',
             body: data,
             credentials: 'include',
