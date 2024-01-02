fc8fd84ca7c6a8c8ba8f239cbfbc7ba4d15d169f
Ayush Roy <84558759+ayushroy-117@users.noreply.github.com>
2024-01-02 11:21:29 +0530

    Update LoginPage.js

--- client/src/Pages/LoginPage.js
+++ client/src/Pages/LoginPage.js
@@ -10,7 +10,7 @@ export default function LoginPage() {
 
     async function login(ev) {
         ev.preventDefault();
-        const response = await fetch('https://talksss.vercel.app/login', {
+        const response = await fetch('https://talks-ashen.vercel.app/login', {
             method: 'POST',
             body: JSON.stringify({ username, password }),
             headers: { 'Content-Type': 'application/json' },
