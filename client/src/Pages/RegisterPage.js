0be4fcb7e23e35574e6839167d7caa3445b5124b
Ayush Roy <84558759+ayushroy-117@users.noreply.github.com>
2024-01-03 00:36:13 +0530

    Update RegisterPage.js

--- client/src/Pages/RegisterPage.js
+++ client/src/Pages/RegisterPage.js
@@ -5,7 +5,7 @@ export default function RegisterPage() {
     const [password, setPassword] = useState('');
     async function register(ev) {
         ev.preventDefault();
-        const response = await fetch('https://talks-ashen.vercel.app//register', {
+        const response = await fetch('https://talksss-ayushroy-117.vercel.app/register', {
             method: 'POST',
             body: JSON.stringify({ username, password }),
             headers: { 'Content-Type': 'application/json' },
