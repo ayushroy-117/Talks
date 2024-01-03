06f58ef13561edc3a8fa732955dac02d697389c7
Ayush Roy <84558759+ayushroy-117@users.noreply.github.com>
2024-01-03 14:35:04 +0530

    Update EditPost.js

--- client/src/Pages/EditPost.js
+++ client/src/Pages/EditPost.js
@@ -11,7 +11,7 @@ export default function EditPost() {
     const [redirect, setRedirect] = useState(false);
 
     useEffect(() => {
-        fetch('https://talks-ashen.vercel.app/post/' + id)
+        fetch('https://talks-rho.vercel.app/post/' + id)
             .then(response => {
                 response.json().then(postInfo => {
                     setTitle(postInfo.title);
